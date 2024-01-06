7bb07d0f352beb4c78abfaa9887771d29801f1ab
Yunus <skysoft.founder@gmail.com>
2024-01-06 18:34:53 +0300

    Initial Commit

---
+++ PickerLibrary2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickerLibrary2
+//
+//  Created by Yunus Can on 06/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickerLibrary2/PickerLibrary2App.swift
@@ -0,0 +1,17 @@
+//
+//  PickerLibrary2App.swift
+//  PickerLibrary2
+//
+//  Created by Yunus Can on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PickerLibrary2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
